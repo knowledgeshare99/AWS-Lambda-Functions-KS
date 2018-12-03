e19f51eef8ab67d07ce06c9e28a3bebadaddbd7c
knowledgeshare99 <32073408+knowledgeshare99@users.noreply.github.com>
2018-12-03 19:01:13 +0530

    Sending email to DL or group
    Sending email to DL or group

---
+++ SendEmail.py
@@ -0,0 +1,35 @@
+
+#https://github.com/thigley986/Lambda-AWS-SES-Send-Email/blob/master/SendEmail.py
+mport boto3
+
+ses = boto3.client('ses')
+
+email_from = 'Email'
+email_to = 'Email'
+email_cc = 'Email'
+emaiL_subject = 'Subject'
+email_body = 'Body'
+
+
+def lambda_handler(event, context):
+    response = ses.send_email(
+        Source = email_from,
+        Destination={
+            'ToAddresses': [
+                email_to,
+            ],
+            'CcAddresses': [
+                email_cc,
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': emaiL_subject
+            },
+            'Body': {
+                'Text': {
+                    'Data': email_body
+                }
+            }
+        }
+    )
